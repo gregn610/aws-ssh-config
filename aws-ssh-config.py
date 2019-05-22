c4423d907875e70bfae9e765a15c4394f24d2516
Greg Nicol <greg.nicol@nominet.uk>
2019-05-22 16:34:54 +0100

    refactor arg parsing out of main()

--- aws-ssh-config.py
+++ aws-ssh-config.py
@@ -27,6 +27,70 @@ BLACKLISTED_REGIONS = [
 ]
 
 
+
+def handle_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--default-user',
+        help='Default ssh username to use'
+             'if it can\'t be detected from AMI name')
+    parser.add_argument(
+        '--keydir',
+        default='~/.ssh/',
+        help='Location of private keys')
+    parser.add_argument(
+        '--no-identities-only',
+        action='store_true',
+        help='Do not include IdentitiesOnly=yes in ssh config; may cause'
+             ' connection refused if using ssh-agent')
+    parser.add_argument(
+        '--postfix',
+        default='',
+        help='Specify a postfix to append to all host names')
+    parser.add_argument(
+        '--prefix',
+        default='',
+        help='Specify a prefix to prepend to all host names')
+    parser.add_argument(
+        '--private',
+        action='store_true',
+        help='Use private IP addresses (public are used by default)')
+    parser.add_argument(
+        '--profile',
+        help='Specify AWS credential profile to use')
+    parser.add_argument(
+        '--proxy',
+        default='',
+        help='Specify a bastion host for ProxyCommand')
+    parser.add_argument(
+        '--region',
+        action='store_true',
+        help='Append the region name at the end of the concatenation')
+    parser.add_argument(
+        '--ssh-key-name',
+        default='',
+        help='Override the ssh key to use')
+    parser.add_argument(
+        '--strict-hostkey-checking',
+        action='store_true',
+        help='Do not include StrictHostKeyChecking=no in ssh config')
+    parser.add_argument(
+        '--tags',
+        help='A comma-separated list of tag names to be considered for'
+             ' concatenation. If omitted, all tags will be used')
+    parser.add_argument(
+        '--user',
+        help='Override the ssh username for all hosts')
+    parser.add_argument(
+        '--white-list-region',
+        default='',
+        help='Which regions must be included. If omitted, all regions'
+             ' are considered',
+        nargs='+')
+    args = parser.parse_args()
+    return args
+
+
 def generate_id(instance, tags_filter, region):
     instance_id = ''
 
@@ -217,72 +281,14 @@ def process_aws(args_profile,
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--default-user',
-        help='Default ssh username to use'
-             'if it can\'t be detected from AMI name')
-    parser.add_argument(
-        '--keydir',
-        default='~/.ssh/',
-        help='Location of private keys')
-    parser.add_argument(
-        '--no-identities-only',
-        action='store_true',
-        help='Do not include IdentitiesOnly=yes in ssh config; may cause'
-             ' connection refused if using ssh-agent')
-    parser.add_argument(
-        '--postfix',
-        default='',
-        help='Specify a postfix to append to all host names')
-    parser.add_argument(
-        '--prefix',
-        default='',
-        help='Specify a prefix to prepend to all host names')
-    parser.add_argument(
-        '--private',
-        action='store_true',
-        help='Use private IP addresses (public are used by default)')
-    parser.add_argument(
-        '--profile',
-        help='Specify AWS credential profile to use')
-    parser.add_argument(
-        '--proxy',
-        default='',
-        help='Specify a bastion host for ProxyCommand')
-    parser.add_argument(
-        '--region',
-        action='store_true',
-        help='Append the region name at the end of the concatenation')
-    parser.add_argument(
-        '--ssh-key-name',
-        default='',
-        help='Override the ssh key to use')
-    parser.add_argument(
-        '--strict-hostkey-checking',
-        action='store_true',
-        help='Do not include StrictHostKeyChecking=no in ssh config')
-    parser.add_argument(
-        '--tags',
-        help='A comma-separated list of tag names to be considered for'
-             ' concatenation. If omitted, all tags will be used')
-    parser.add_argument(
-        '--user',
-        help='Override the ssh username for all hosts')
-    parser.add_argument(
-        '--white-list-region',
-        default='',
-        help='Which regions must be included. If omitted, all regions'
-             ' are considered',
-        nargs='+')
-    args = parser.parse_args()
+    args = handle_args()
 
     print('# Generated on ' + time.asctime(time.localtime(time.time())))
     print('# ' + ' '.join(sys.argv))
     print('# ')
     print('')
 
-    ret = process_aws(
+    config_list = process_aws(
         args.profile,
         args.tags,
         args.region,
@@ -293,14 +299,13 @@ def main():
         args.prefix,
         args.postfix, )
 
-    for (ami_image_id, host_id, instance_id, image_id, key_name, ip_addr) in ret:
+    for (ami_image_id, host_id, instance_id, image_id, key_name, ip_addr) in config_list:
         print_config(ami_image_id, host_id, instance_id, image_id, key_name, ip_addr,
                      args.keydir,
                      args.ssh_key_name,
                      args.no_identities_only,
                      args.strict_hostkey_checking,
                      args.proxy)
-
 
 if __name__ == '__main__':
     main()
